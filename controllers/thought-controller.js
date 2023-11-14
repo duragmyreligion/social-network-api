db689c9517ba293a0c3e5287b54ed4dfffcce3bb
DuragMyReligion <samuelkininmonth@hotmail.com>
2023-11-14 14:19:44 +0800

    Updated error checking with Update Thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -51,16 +51,18 @@ const thoughtController = {
   },
   // update thought
   async updateThought(req, res) {
-    const dbThoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true });
-
-    if (!dbThoughtData) {
-      return res.status(404).json({ message: 'No thought with this id!' });
+    try {
+      const dbThoughtData = await Thought.findOneAndUpdate({ _id: req.params.thoughtId }, { $set: req.body }, { runValidators: true, new: true });
+  
+      if (!dbThoughtData) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+  
+      res.json(dbThoughtData);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal Server Error' });
     }
-
-    res.json(dbThoughtData);
-
-    console.log(err);
-    res.status(500).json(err);
   },
   // delete thought
   async deleteThought(req, res) {
